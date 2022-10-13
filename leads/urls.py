22c05dfd27b3accda12ada19d5628e2d8d9f4c60
patilaniket04@gmail.com <patilaniket04@gmail.com>
2022-10-13 18:54:19 +0530

    Created views and templates

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import lead_list, lead_details, lead_create
 
 urlpatterns = [
     
-    path('',home)
+    path('',lead_list,name='lead_list'),
+    path('lead_details/<int:pk>',lead_details,name='lead_detail'),
+    path('lead-create',lead_create,name='lead_create')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead
+from .forms import LeadForm
 
 # Create your views here.
-def home(request):
+def lead_list(request):
     data = Lead.objects.all()
     context = {"data":data}
-    return render(request,"leads/index.html", context)
+    return render(request,"leads/lead_list.html", context)
 
+def lead_details(request, pk):
+    lead = Lead.objects.get(id=pk)
+    return render(request,"leads/lead_details.html", {"lead":lead})
+
+def lead_create(request):
+    context={
+        "form":LeadForm()
+    }
+    return render(request,"leads/create_lead.html",context)
+    
+    
